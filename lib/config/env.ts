f109d8d1975bde776fe54965b662404585124a69
rezwanahamed <rejwanahamed85@gmail.com>
2024-11-13 17:01:52 +0600

    env type septy added in env config

--- lib/config/env.ts
+++ lib/config/env.ts
@@ -1,9 +1,9 @@
 export const envConfig = Object.freeze({
-    // Client configuration
-    ENCRYPTION_SECRET_KEY: process.env.NEXT_PUBLIC_API_URL as string,
-  
-    // Server configuration
-    BACKEND_SERVER_URL: process.env.NEXT_PUBLIC_API_URL as string,
-  });
-  
-  
+  // Client configuration
+  ENCRYPTION_SECRET_KEY: process.env.NEXT_PUBLIC_API_URL as string,
+  NEXT_AUTH_URL: process.env.NEXTAUTH_URL as string,
+  NEXT_AUTH_SECRET_KEY: process.env.NEXTAUTH_SECRET as string,
+
+  // Server configuration
+  BACKEND_SERVER_URL: process.env.NEXT_PUBLIC_API_URL as string,
+});
